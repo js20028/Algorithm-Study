23f40376adcfb3e585e24b0c0180909fc2629c76
js20028 <68904961+js20028@users.noreply.github.com>
2021-10-13 23:47:38 +0900

    Update 1-8.py

--- 1-8.py
+++ 1-8.py
@@ -1,7 +1,13 @@
 def solution(nums):
-    answer = -1
-
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
-
+    answer = 0
+    for i in range(len(nums)-2):
+        for j in range(i+1,len(nums)-1):
+            for k in range(j+1,len(nums)):
+                if (i+j+k) % 2 != 0:
+                    for l in range(2,(i+j+k)//2+1):
+                        if (i+j+k) % l == 0:
+                            break
+                        elif l==((i+j+k)//2):
+                            answer += 1
+                            
     return answer
