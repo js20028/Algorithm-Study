7eeb65f93c2f67a9900d9a4a77ef86d9d4f1d6d0
js20028 <68904961+js20028@users.noreply.github.com>
2021-10-13 23:54:15 +0900

    Update 1-8.py

--- 1-8.py
+++ 1-8.py
@@ -3,11 +3,10 @@ def solution(nums):
     for i in range(len(nums)-2):
         for j in range(i+1,len(nums)-1):
             for k in range(j+1,len(nums)):
-                if (i+j+k) % 2 != 0:
-                    for l in range(2,(i+j+k)//2+1):
-                        if (i+j+k) % l == 0:
+                if (nums[i]+nums[j]+nums[k]) % 2 != 0:
+                    for l in range(2,(nums[i]+nums[j]+nums[k])//2+1):
+                        if (nums[i]+nums[j]+nums[k]) % l == 0:
                             break
-                        elif l==((i+j+k)//2):
+                        elif l==((nums[i]+nums[j]+nums[k])//2):
                             answer += 1
-                            
     return answer
