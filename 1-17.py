89703604fb9c6d3434e761bd3851a33b1cd8e12f
js20028 <68904961+js20028@users.noreply.github.com>
2021-10-28 17:11:31 +0900

    Update 1-17.py

--- 1-17.py
+++ 1-17.py
@@ -1,3 +1,11 @@
 def solution(d, budget):
     answer = 0
+    sum = 0
+    for i in sorted(d):
+        sum += i
+        answer += 1
+        if sum > budget:
+            answer -= 1
+            break
+            
     return answer
